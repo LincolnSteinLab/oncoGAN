22516f57b5809a6f3ccbdfdd38b7c4610921c331
anderdnavarro <ander.d.navarro@gmail.com>
2024-10-08 16:18:29 -0400

    Update matrix transformation

--- deeptumour/src/utils.py
+++ deeptumour/src/utils.py
@@ -1,7 +1,9 @@
 import os
 import re
+import pickle
 import pandas as pd
 import allel
+import math
 from Bio.Seq import reverse_complement
 from liftover import ChainFile
 from pyfaidx import Fasta
@@ -25,6 +27,65 @@ def hg38tohg19(vcf:pd.DataFrame) -> pd.DataFrame:
 
     return(vcf)
 
+def process_dnp(vcf:pd.DataFrame) -> pd.DataFrame:
+
+    """
+    Split DNPs into two different SNPs
+    """
+    
+    # Select DNPs positions
+    dnps = vcf[(vcf['REF'].str.len() == 2) & (vcf['ALT'].str.len() == 2)]
+
+    # Duplicate the DNPs and create the second SNP
+    dnps_dup = dnps.copy()
+    dnps_dup['REF'] = dnps_dup['REF'].str[1]
+    dnps_dup['ALT'] = dnps_dup['ALT'].str[1]
+    dnps_dup['POS'] = dnps_dup['POS'] + 1
+    dnps_dup['is_snp'] = True
+
+    # Modify the original DNPs to create the first SNP
+    vcf.loc[dnps.index, 'REF'] = dnps['REF'].str[0]
+    vcf.loc[dnps.index, 'ALT'] = dnps['ALT'].str[0]
+    vcf.loc[dnps.index, 'is_snp'] = True
+
+    # Concatenate the original df with the duplicated rows
+    new_vcf = pd.concat([vcf, dnps_dup], ignore_index=True).sort_values(by=['CHROM', 'POS'])
+
+    return(new_vcf.reset_index(drop=True))
+
+def process_tnp(vcf:pd.DataFrame) -> pd.DataFrame:
+
+    """
+    Split TNPs into three different SNPs
+    """
+    
+    # Select TNPs positions
+    tnps = vcf[(vcf['REF'].str.len() == 3) & (vcf['ALT'].str.len() == 3)]
+
+    # Duplicate the TNPs and create the second SNP
+    tnps_dup_second = tnps.copy()
+    tnps_dup_second['REF'] = tnps_dup_second['REF'].str[1]
+    tnps_dup_second['ALT'] = tnps_dup_second['ALT'].str[1]
+    tnps_dup_second['POS'] = tnps_dup_second['POS'] + 1
+    tnps_dup_second['is_snp'] = True
+
+    # Duplicate the TNPs and create the third SNP
+    tnps_dup_third = tnps.copy()
+    tnps_dup_third['REF'] = tnps_dup_third['REF'].str[2]
+    tnps_dup_third['ALT'] = tnps_dup_third['ALT'].str[2]
+    tnps_dup_third['POS'] = tnps_dup_third['POS'] + 2 
+    tnps_dup_third['is_snp'] = True
+
+    # Modify the original TNPs to create the first SNP
+    vcf.loc[tnps.index, 'REF'] = tnps['REF'].str[0]
+    vcf.loc[tnps.index, 'ALT'] = tnps['ALT'].str[0]
+    vcf.loc[tnps.index, 'is_snp'] = True
+
+    # Concatenate the original df with the duplicated rows
+    new_vcf = pd.concat([vcf, tnps_dup_second, tnps_dup_third], ignore_index=True).sort_values(by=['CHROM', 'POS'])
+
+    return(new_vcf.reset_index(drop=True))
+
 def vcf2df(vcf:os.path, prefix:bool, liftOver:bool) -> pd.DataFrame:
     
     """
@@ -32,7 +93,8 @@ def vcf2df(vcf:os.path, prefix:bool, liftOver:bool) -> pd.DataFrame:
     """
 
     # Open VCF
-    vcf:pd.DataFrame = allel.vcf_to_dataframe(vcf, fields='*', alt_number=2)
+    vcf:pd.DataFrame = allel.vcf_to_dataframe(vcf, fields='*', alt_number=1)
+    vcf.drop_duplicates(inplace=True)
     vcf.reset_index(drop=True, inplace=True)
 
     # LiftOver coordinates if the original VCF is in hg38
@@ -53,9 +115,12 @@ def vcf2df(vcf:os.path, prefix:bool, liftOver:bool) -> pd.DataFrame:
     else:
         pass
 
+    # Split DNPs and TNPs
+    vcf = process_dnp(vcf)
+    vcf = process_tnp(vcf)
+
     # Filter SNVs in chr1-chr22
-    vcf_filter:pd.DataFrame = vcf[(vcf['is_snp'] == True) & (vcf['CHROM'].isin(chr_list))]
-    print(f"\n--- {len(vcf_filter)} SNVs from a total of {len(vcf)} variants in the VCF file ---\n")
+    vcf_filter:pd.DataFrame = vcf[(vcf['is_snp'] == True) & (vcf['CHROM'].isin(chr_list)) & (vcf['REF'] != '-') & (vcf['ALT'] != '-')]
 
     return(vcf_filter.reset_index(drop=True))
 
@@ -73,7 +138,7 @@ def df2bins(df:pd.DataFrame, sample_name:str, prefix:bool) -> pd.DataFrame:
         header_bins.iloc[:, 0] = header_bins.iloc[:, 0].apply(lambda x: str(x).replace('chr', ''))
 
     # Get bins from the df
-    df_bins:pd.Series = df.CHROM + '.' + df.POS.apply(lambda x: int(round(float(x) / 1000000))).astype(str)
+    df_bins:pd.Series = df.CHROM + '.' + df.POS.apply(lambda x: int(math.floor(float(x) / 1000000))).astype(str)
     bins:pd.DataFrame = pd.DataFrame({'bins': pd.Series(pd.Categorical(df_bins, categories=header_bins.iloc[:, 0]))})
     
     # Group bins and count
@@ -90,13 +155,17 @@ def df2mut(df:pd.DataFrame, sample_name:str, fasta:Fasta) -> pd.DataFrame:
     # Load the header of the mutation types
     header_muts:pd.DataFrame = pd.read_csv('/DeepTumour/trained_models/Mut-Type-Header.csv')
 
+    # Load Z-Norm parameters
+    with open("/DeepTumour/trained_models/z-norm.pkl", "rb") as f:
+        z_norm:dict = pickle.load(f)
+
     # Extract the mutation types
     changes:list = []
     for _,row in df.iterrows():
         chrom:str = str(row['CHROM'])
         pos:int = int(row['POS'])
         ref:str = row['REF']
-        alt:str = row['ALT_1']
+        alt:str = row['ALT']
         ref_ctx:str = fasta[chrom][pos-2:pos+1].seq.upper()
 
         # Check that we have the same reference bases
@@ -133,6 +202,9 @@ def df2mut(df:pd.DataFrame, sample_name:str, fasta:Fasta) -> pd.DataFrame:
         mutations.loc[0:5, sample_name] = sgl_prop
         mutations.loc[6:53, sample_name] = di_prop
         mutations.loc[54:149, sample_name] = tri_prop
+
+        ## z-norm
+        mutations[mutations.columns[1:]] = mutations.apply(lambda x: (x[1:] - z_norm[x['bins']]['mean']) / z_norm[x['bins']]['std'], axis=1)
 
     return(mutations)
 
